5dc7dbe1277176c6ebc9574dec37f8e960ed0a51
Felizxfell <felizxhgf@gmial.com>
2022-06-03 14:48:23 -0500

    quite un espacio

--- resources/js/Pages/AboutMe/FormAboutMe.vue
+++ resources/js/Pages/AboutMe/FormAboutMe.vue
@@ -4,7 +4,7 @@
             <div class="mb-4">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="txtaboutme">
                     Mi informacion principal
-                </label>                        
+                </label>
                 <textarea 
                     class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
                     id="txtaboutme"  placeholder="Info here..." autocomplete="off"
